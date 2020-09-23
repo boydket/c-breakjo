62fb6093b82d79902fb02e6d91ef3e4048993816
boydket <boydket@naver.com>
2020-09-23 13:07:54 +0900

    Add files via upload

---
+++ 20150254_OHG/week5/(p.130)RacingMain.cpp
@@ -0,0 +1,14 @@
+#include "(p.128~129)Car.h"
+
+int main(void)
+{
+	Car run99;
+	run99.InitMembers("run99", 100);
+	run99.Accel();
+	run99.Accel();
+	run99.Accel();
+	run99.ShowCarState();
+	run99.Break();
+	run99.ShowCarState();
+	return 0;
+}
