e9741e8d085aa1bab4056fbcb635e55f4568787f
pyjpjw <71378451+pyjpjw@users.noreply.github.com>
2020-09-25 21:45:53 +0900

    Create borrow system.h

---
+++ 20170757_PYJ/borrow system.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class borrow_system {
+private: // 변수
+	int selection;
+	int height;
+	int weight;
+	int vest_price = 0;
+	int tube_price = 0;
+	int borrow_time = 0;
+
+
+
+public: // 함수
+
+	borrow_system();
+
+};
